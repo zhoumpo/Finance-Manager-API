049fa87fa36773a386a7abbd22bc78e60b07dc17
Zhoumpo <niamor.jeannes@gmail.com>
2023-07-02 19:58:21 +0200

    Add first app and settings folder

---
+++ finance_manager/models.py
@@ -0,0 +1,64 @@
+from django.db import models
+
+
+class AccountType(models.Model):
+    TYPE_CHOICES = (
+        ('current_account', 'Current Account'),
+        ('saving_account', 'Saving Account'),
+        ('life_assurance', 'Life Assurance'),
+    )
+
+    name = models.CharField(max_length=50, choices=TYPE_CHOICES)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name_plural = "Account Types"
+
+
+class Account(models.Model):
+    name = models.CharField(max_length=50)
+    account_type = models.ForeignKey(AccountType, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name_plural = "Accounts"
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name_plural = "Categories"
+
+
+class Subcategory(models.Model):
+    name = models.CharField(max_length=50)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name_plural = "Subcategories"
+
+
+class Transaction(models.Model):
+    date = models.DateField()
+    description = models.CharField(max_length=100)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    subcategory = models.ForeignKey(Subcategory, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.description
+    
+    class Meta:
+        verbose_name_plural = "Transactions"
--- finance_manager_api/settings/common.py
+++ finance_manager_api/settings/common.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -21,11 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure--+h3z_5rc-kzxscxwa%mhhqt=x5wmal2oa9^h@fzgeep%$()h1'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
 
 
 # Application definition
