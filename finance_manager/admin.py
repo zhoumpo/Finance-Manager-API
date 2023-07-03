7ede99eca6e635e79e8086ceb7a0ce7b31dfd0a9
Zhoumpo <niamor.jeannes@gmail.com>
2023-07-03 12:58:30 +0200

    Register models in admin

--- finance_manager/admin.py
+++ finance_manager/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Account, Bank, Category, Transaction
 
-# Register your models here.
+
+admin.site.register(Bank)
+admin.site.register(Account)
+admin.site.register(Category)
+admin.site.register(Transaction)
