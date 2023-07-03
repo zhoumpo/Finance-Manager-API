bb363b10f6566929b0fc4055ea224f49eeebaf83
Zhoumpo <niamor.jeannes@gmail.com>
2023-07-03 12:51:38 +0200

    Update models.py

--- finance_manager/models.py
+++ finance_manager/models.py
@@ -1,19 +1,31 @@
 from django.db import models
 
 
-class Account(models.Model):
-    TYPE_CHOICES = (
-        ('current_account', 'Current Account'),
-        ('saving_account', 'Saving Account'),
-        ('life_assurance', 'Life Assurance'),
-    )
-
-    name = models.CharField(max_length=50)
-    account_type = models.CharField(max_length=50, choices=TYPE_CHOICES)
+class Bank(models.Model):
+    name = models.CharField(max_length=256)
+    country = models.CharField(max_length=256)
 
     def __str__(self):
         return self.name
-    
+
+    class Meta:
+        verbose_name_plural = "Banks"
+
+
+class Account(models.Model):
+    TYPE_CHOICES = (
+        ("current_account", "Current Account"),
+        ("saving_account", "Saving Account"),
+        ("life_assurance", "Life Assurance"),
+    )
+
+    name = models.CharField(max_length=256)
+    bank = models.ForeignKey(Bank, on_delete=models.CASCADE)
+    account_type = models.CharField(max_length=256, choices=TYPE_CHOICES)
+
+    def __str__(self):
+        return self.name
+
     class Meta:
         verbose_name_plural = "Accounts"
 
@@ -23,32 +35,28 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
-    
+
     class Meta:
         verbose_name_plural = "Categories"
 
 
-class Subcategory(models.Model):
-    name = models.CharField(max_length=50)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.name
-    
-    class Meta:
-        verbose_name_plural = "Subcategories"
-
-
 class Transaction(models.Model):
     date = models.DateField()
-    description = models.CharField(max_length=100)
+    account = models.ForeignKey("Account", on_delete=models.CASCADE)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
-    account = models.ForeignKey(Account, on_delete=models.CASCADE)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    subcategory = models.ForeignKey(Subcategory, on_delete=models.CASCADE, null=True, blank=True)
+    comment = models.TextField()
+    category_1 = models.ForeignKey(
+        "Category", on_delete=models.CASCADE, related_name="category_1", null=True
+    )
+    category_2 = models.ForeignKey(
+        "Category", on_delete=models.CASCADE, related_name="category_2", null=True
+    )
+    category_3 = models.ForeignKey(
+        "Category", on_delete=models.CASCADE, related_name="category_3", null=True
+    )
 
     def __str__(self):
-        return self.description
-    
+        return self.account + " - " + str(self.amount)
+
     class Meta:
         verbose_name_plural = "Transactions"
