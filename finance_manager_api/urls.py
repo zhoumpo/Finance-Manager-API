7bf1def50fa7deb86c054be7c8d105cc7b96e53d
Zhoumpo <niamor.jeannes@gmail.com>
2023-07-03 13:03:55 +0200

    Add restframework and base urls

--- finance_manager_api/urls.py
+++ finance_manager_api/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from finance_manager import urls as finance_manager_urls
+
+
+router = routers.DefaultRouter()
+router.registry.extend(finance_manager_urls.router.registry)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("api/", include(router.urls)),
 ]
